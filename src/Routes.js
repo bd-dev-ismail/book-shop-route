08bf8d47a8ce8e587f2f1a030391b5351d79961b
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-06 16:24:36 +0600

    error page create

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import About from "./components/About/About";
 import Books from "./components/Books/Books";
+import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
 import Pricing from "./components/Pricing/Pricing";
 import Main from "./layout/Main";
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <Error></Error>,
     children: [
       { path: "/", element: <Home></Home> },
       { path: "/home", element: <Home></Home> },
---
+++ src/components/Error/Error.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+const Error = () => {
+    const error = useRouteError();
+    return (
+      <div className='container mx-auto my-6'>
+        <div className="alert alert-warning shadow-lg">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span>Warning: Invalid url address! Error:</span>
+            <p>{error.statusText || error.message}</p>
+            <p>{error.status}</p>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Error;
