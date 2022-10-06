e22fce8219dc0064fc6dfd5b109edc840b27c5ed
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-06 12:47:12 +0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
